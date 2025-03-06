29d4ee6904947fa1dc9dd9c1655511f6b1d41c6b
Dominik Kownacki <dominik.kownacki@actispro.pl>
2025-03-06 16:55:07 +0100

    Added styles to views, added guid is not empty converter

---
+++ SubscriptionManager.UI/Converters/GuidIsNotEmptyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace SubscriptionManager.UI.Converters;
+
+public class GuidIsNotEmptyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Guid guid)
+        {
+            return guid != Guid.Empty;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
